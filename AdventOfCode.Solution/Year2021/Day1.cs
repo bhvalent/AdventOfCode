1cbca72bd275a9f0018f2538cb66eda48e17b5b7
bhvalent <benhudvalentine@gmail.com>
2021-12-03 16:55:32 -0600

    Finished Day 1.2

--- AdventOfCode.Solution/Year2021/Day1.cs
+++ AdventOfCode.Solution/Year2021/Day1.cs
@@ -11,6 +11,25 @@ namespace AdventOfCode.Solution.Year2021
             _csvHelper = csvHelper ?? throw new ArgumentNullException(nameof(csvHelper));
         }
 
+        /*
+            As the submarine drops below the surface of the ocean, 
+            it automatically performs a sonar sweep of the nearby sea floor. 
+            On a small screen, the sonar sweep report (your puzzle input) appears: 
+            each line is a measurement of the sea floor depth as the sweep looks 
+            further and further away from the submarine.
+
+            This report indicates that, scanning outward from the submarine, 
+            the sonar sweep found depths of 199, 200, 208, 210, and so on.
+
+            The first order of business is to figure out how quickly the depth increases, 
+            just so you know what you're dealing with - you never know if the keys will 
+            get carried into deeper water by an ocean current or a fish or something.
+
+            To do this, count the number of times a depth measurement increases from 
+            the previous measurement. (There is no measurement before the first measurement.) 
+            
+            How many measurements are larger than the previous measurement?
+        */
         public int SonarSweep(List<int> measurements)
         {
             return GetNumberOfIncreases(measurements);
@@ -22,6 +41,26 @@ namespace AdventOfCode.Solution.Year2021
             return GetNumberOfIncreases(measurements);
         }
 
+        /*
+            Considering every single measurement isn't as useful as you expected: there's just too much noise in the data.
+
+            Instead, consider sums of a three-measurement sliding window.
+
+            Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum.
+
+            How many sums are larger than the previous sum?
+        */
+        public int SonarSweepWithWindow(List<int> measurements)
+        {
+            return GetSlidingWindowIncreases(measurements);
+        }
+
+        public int SonarSweepWithWindow(string filepath)
+        {
+            var measurements = _csvHelper.GetListOfInt(filepath);
+            return GetSlidingWindowIncreases(measurements);
+        }
+
         private int GetNumberOfIncreases(List<int> measurements)
         {
             if (measurements == null || measurements.Count < 2) return 0;
@@ -31,6 +70,23 @@ namespace AdventOfCode.Solution.Year2021
             {
                 if (i == 0) continue;
                 if (measurements[i] > measurements[i -1]) increases++;
+            }
+            return increases;
+        }
+
+        private int GetSlidingWindowIncreases(List<int> measurements)
+        {
+            if (measurements == null || measurements.Count < 2) return 0;
+            
+            var increases = 0;
+            int? firstSum = null;
+            int? secondSum = null;
+            for (int i = 0; i < measurements.Count; i++)
+            {
+                if (i < 2) continue;
+                secondSum = measurements[i] + measurements[i - 1] + measurements[i - 2];
+                if (firstSum.HasValue && firstSum < secondSum) increases++;
+                firstSum = secondSum;
             }
             return increases;
         }
--- AdventOfCode.Test/Year2021/Day1Tests.cs
+++ AdventOfCode.Test/Year2021/Day1Tests.cs
@@ -96,6 +96,116 @@ public class Day1Tests
         Assert.Equal(1521, result);
     }
 
+    [Fact]
+    public void Day1_SonarSweepWithWindow_ShouldReturnZeroWhenPassedNull()
+    {
+        // arrange
+        var target = new Day1(_mockCsvHelper.Object);
+
+        // act
+        var result = target.SonarSweepWithWindow((List<int>)null);
+
+        // assert
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Day1_SonarSweepWithWindow_ShouldReturnZeroWhenPassedEmptyList()
+    {
+        // arrange
+        var measurements = new List<int>();
+        var target = new Day1(_mockCsvHelper.Object);
+
+        // act
+        var result = target.SonarSweepWithWindow(measurements);
+
+        // assert
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Day1_SonarSweepWithWindow_ShouldReturnZeroWhenPassedListOf1()
+    {
+        // arrange
+        var measurements = new List<int> { 1 };
+        var target = new Day1(_mockCsvHelper.Object);
+
+        // act
+        var result = target.SonarSweepWithWindow(measurements);
+
+        // assert
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Day1_SonarSweepWithWindow_ShouldReturnZeroWhenPassedListOf2()
+    {
+        // arrange
+        var measurements = new List<int> { 1, 2 };
+        var target = new Day1(_mockCsvHelper.Object);
+
+        // act
+        var result = target.SonarSweepWithWindow(measurements);
+
+        // assert
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Day1_SonarSweepWithWindow_ShouldReturnZeroWhenPassedListOf3()
+    {
+        // arrange
+        var measurements = new List<int> { 1, 2, 3 };
+        var target = new Day1(_mockCsvHelper.Object);
+
+        // act
+        var result = target.SonarSweepWithWindow(measurements);
+
+        // assert
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Day1_SonarSweepWithWindow_ShouldReturnResultWithOneIncrease()
+    {
+        // arrange
+        var measurements = new List<int> { 1, 2, 3, 4 };
+        var target = new Day1(_mockCsvHelper.Object);
+
+        // act
+        var result = target.SonarSweepWithWindow(measurements);
+
+        // assert
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void Day1_SonarSweepWithWindow_ShouldReturnResultWithOneIncreaseAndDecrease()
+    {
+        // arrange
+        var measurements = new List<int> { 1, 2, 3, 4, 0 };
+        var target = new Day1(_mockCsvHelper.Object);
+
+        // act
+        var result = target.SonarSweepWithWindow(measurements);
+
+        // assert
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void Day1_SonarSweepWithWindow_ToGetResults()
+    {
+        // arrange
+        var target = GetTarget();
+
+        // act
+        var result = target.SonarSweepWithWindow("TestData/SonarSweep.csv");
+
+        // assert
+        Assert.Equal(1543, result);
+    }
+
     private Day1 GetTarget()
     {
         return new Day1(new CsvService(new CsvHelperWrapper()));
